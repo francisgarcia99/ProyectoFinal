13143f056c5c1a903b6bcd2f5f07ebd2872c2ebd
francis <francisgaarcia@gmail.com>
2024-04-21 17:27:13 -0600

    modificacion del login

--- proyecto/usuario/models.py
+++ proyecto/usuario/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 # Create your models here.
+
+def validate_telefono_length(value):
+    if len(str(value)) != 8:
+        raise ValidationError('El número de teléfono debe tener exactamente 8 dígitos.')
+def validate_dpi_length(value):
+    if len(str(value)) != 13:
+        raise ValidationError('El número de teléfono debe tener exactamente 13 dígitos.')
+def validate_imagen_presence(value):
+    if not value:
+        raise ValidationError('Se requiere proporcionar una imagen.')
+
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
-    dpi=models.IntegerField()
+
+    dpi = models.BigIntegerField(validators=[validate_dpi_length,])   
+    #ALTER TABLE usuario_usuario ALTER COLUMN dpi TYPE BIGINT;
     fecha_nacimiento=models.CharField(max_length=10)
-    telefono=models.IntegerField()
+    telefono = models.IntegerField(validators=[validate_telefono_length,])
     email=models.EmailField(default="example@example.com") 
-    imagen=models.ImageField(upload_to="alumnos", null=True, blank=True)
+    imagen = models.ImageField(upload_to="usuarios", null=True, blank=True)
     created=models.DateField(auto_now_add=True)
     updated=models.DateField(auto_now_add=True)
 
@@ -19,3 +34,6 @@ class Usuario(models.Model):
 
     def _str_(self):
         return self.nombre
+    
+
+
